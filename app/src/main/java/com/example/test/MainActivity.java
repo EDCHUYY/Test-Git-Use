3710cd84ee5a3572d11369227835ce4567c32838
Como_desee <2981006967@qq.com>
2021-06-22 17:39:12 +0800

    Third change

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.test;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView1=findViewById(R.id.text1);
+        CountDownTimer timer=new CountDownTimer(3000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                textView1.setText("Finished1");
+            }
+        };
+        timer.start();
     }
 }
